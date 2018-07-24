f0809e7b381b774cf032f11b1097fac7e45b2d08
Curt Caldwell and Eliot Charette <curt+eliot@>
2018-07-24 11:38:16 -0700

    add view for drinks

--- CoffeeShop/Controllers/DrinksController.cs
+++ CoffeeShop/Controllers/DrinksController.cs
@@ -1,30 +1,30 @@
-// using Microsoft.AspNetCore.Mvc;
-// using System.Collections.Generic;
-// using CoffeeShop.Models;
-// using System;
-//
-// namespace CoffeeShop.Controllers
-// {
-//   public class DrinksController : Controller
-//   {
-//     [HttpGet("/drinks")]
-//     public ActionResult Index()
-//     {
-//       List<Drink> all = Drink.GetAll();
-//       return View(Drink.GetAll());
-//     }
-//
-//     [HttpPost("/drinks")]
-//     public ActionResult CollectInfo(string newDrink)
-//     {
-//       Drink newDrink = new Drink(newDrink);
-//       newDrink.Save();
-//       return RedirectToAction("Index");
-//     }
-//     [HttpGet("/drinks/new")]
-//     public ActionResult CreateForm()
-//     {
-//       return View(Drink.GetAll());
-//     }
-//   }
-// }
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CoffeeShop.Models;
+using System;
+
+namespace CoffeeShop.Controllers
+{
+  public class DrinksController : Controller
+  {
+    [HttpGet("/drinks")]
+    public ActionResult Index()
+    {
+      List<Drink> all = Drink.GetAll();
+      return View(Drink.GetAll());
+    }
+
+    [HttpPost("/drinks")]
+    public ActionResult CollectInfo()
+    {
+      Drink newDrink = new Drink(Request.Form["drink"]);
+      newDrink.Save();
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/drinks/new")]
+    public ActionResult CreateForm()
+    {
+      return View(Drink.GetAll());
+    }
+  }
+}
