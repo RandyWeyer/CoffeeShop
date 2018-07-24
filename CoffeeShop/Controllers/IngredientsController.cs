cde172b4893159001cf32863d66d74d4953c145e
Guest User <Guest@epicodus-26.local>
2018-07-24 16:43:45 -0700

    add ingredients list

--- CoffeeShop/Controllers/IngredientsController.cs
+++ CoffeeShop/Controllers/IngredientsController.cs
@@ -7,7 +7,7 @@ namespace CoffeeShop.Controllers
 {
   public class IngredientsController : Controller
   {
-    [HttpGet("/ingredients")]
+    [HttpGet("/ingredients/new")]
     public ActionResult IngredientForm()
     {
       Dictionary<string,object> model = new Dictionary<string,object>{};
@@ -18,6 +18,25 @@ namespace CoffeeShop.Controllers
       model.Add("inventories", allInventories);
 
       return View(model);
+    }
+    [HttpGet("/ingredients")]
+    public ActionResult Index()
+    {
+      Dictionary<string,object> model = new Dictionary<string,object>{};
+      // Ingredient newIngredient = new Ingredient(drink, item, amount);
+      List<Inventory> allInventories = Inventory.GetAll();
+      List<Drink> allDrinks = Drink.GetAll();
+      model.Add("drinks", allDrinks);
+      model.Add("inventories", allInventories);
+      return View(model);
+    }
+
+    [HttpPost("/ingredients")]
+    public ActionResult CollectInfo()
+    {
+      Ingredient newIngredient = new Ingredient(int.Parse(Request.Form["drink"]), int.Parse(Request.Form["item"]), int.Parse(Request.Form["amount"]));
+      newIngredient.Save();
+      return RedirectToAction("Index");
     }
   }
 }
