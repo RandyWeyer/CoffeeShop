6cb28201d8d3fae89629c9ce0a1e168610b7934b
Guest User <Guest@epicodus-55.local>
2018-07-26 13:37:24 -0700

    updated sql

--- coffeeshop.sql
+++ coffeeshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 23, 2018 at 10:29 PM
+-- Generation Time: Jul 26, 2018 at 10:32 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -33,6 +33,23 @@ CREATE TABLE `drinks` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `drinks`
+--
+
+INSERT INTO `drinks` (`id`, `name`) VALUES
+(1, 'Latte'),
+(2, 'Cappuccino'),
+(3, 'Iced Latte'),
+(4, 'Mocha'),
+(5, 'Vanilla Latte'),
+(6, 'Americano'),
+(7, 'Latte-16oz'),
+(8, 'Iced Latte-16oz'),
+(9, 'Mocha-16oz'),
+(10, 'Vanilla Latte-16oz'),
+(11, 'Americano-16oz');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +63,36 @@ CREATE TABLE `ingredients` (
   `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Dumping data for table `ingredients`
+--
+
+INSERT INTO `ingredients` (`id`, `drink_id`, `inventory_id`, `amount`) VALUES
+(1, 1, 1, 1),
+(2, 1, 2, 1),
+(3, 2, 1, 1),
+(4, 2, 2, 1),
+(5, 3, 1, 2),
+(6, 3, 2, 1),
+(7, 4, 1, 2),
+(8, 4, 2, 1),
+(9, 4, 3, 2),
+(10, 5, 1, 2),
+(11, 5, 2, 1),
+(12, 5, 4, 2),
+(13, 6, 2, 3),
+(14, 7, 1, 2),
+(15, 7, 2, 2),
+(16, 8, 1, 2),
+(17, 8, 2, 2),
+(18, 9, 1, 3),
+(19, 9, 2, 2),
+(20, 9, 3, 3),
+(21, 10, 1, 2),
+(22, 10, 2, 2),
+(23, 10, 4, 3),
+(24, 11, 2, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +104,16 @@ CREATE TABLE `inventories` (
   `item` varchar(255) NOT NULL,
   `item_amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `inventories`
+--
+
+INSERT INTO `inventories` (`id`, `item`, `item_amount`) VALUES
+(1, 'Milk', 2887),
+(2, 'Coffee', 2882),
+(3, 'Chocolate', 2983),
+(4, 'Vanilla', 2666);
 
 --
 -- Indexes for dumped tables
@@ -88,17 +145,17 @@ ALTER TABLE `inventories`
 -- AUTO_INCREMENT for table `drinks`
 --
 ALTER TABLE `drinks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `inventories`
 --
 ALTER TABLE `inventories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
